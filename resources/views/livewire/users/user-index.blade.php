8736728309368164970f009578ab0965396f48f0
sadiakhatun193 <sadiakhatun193@gmail.com>
2025-07-13 17:13:59 +0600

    first commit

---
+++ resources/views/livewire/users/user-index.blade.php
@@ -0,0 +1,10 @@
+<div>
+
+
+    <div class="relative mb-6 w-full">
+    <flux:heading size="xl" level="1">{{ __('Users') }}</flux:heading>
+    <flux:subheading size="lg" class="mb-6">{{ __('Manage your profile ') }}</flux:subheading>
+    <flux:separator variant="subtle" />
+</div>
+
+</div>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Livewire\Volt\Volt;
+use App\Livewire\Users\UserIndex;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+     Route::get("users", UserIndex::class)->name('users.index');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
